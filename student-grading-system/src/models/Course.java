9a5b389ac593137bc8d3f96a20bc6c01ded33f25
Clement Osei Asante <clementoseiasante.nss@gmail.com>
2025-07-07 16:58:54 +0000

    project structure

---
+++ student-grading-system/src/models/Course.java
@@ -0,0 +1,9 @@
+package models;
+
+public class Course {
+    private String courseCode;
+    public
+    public String courseName;
+
+
+}
--- student-grading-system/src/models/Student.java
+++ student-grading-system/src/models/Student.java
@@ -5,60 +5,6 @@ import java.util.ArrayList;
 public class Student {
     public int studentId;
     public String name;
-    private ArrayList<ExamScore> _grades;
 
-    public Student(int id, String studentName){
-        this.name = studentName;
-        this.studentId = id;
-        this._grades = new ArrayList<>();
-    }
-
-    public double calculateAverage(){
-        double total = this.getTotalScore();
-        return total/this._grades.size();
-    }
-
-    public double getHighestGrade(){
-        double highest = 0;
-        for(ExamScore exScore : this._grades){
-            if(exScore.getScore() > highest){
-                highest = exScore.getScore();
-            }
-        }
-        return highest;
-    }
-
-    public double getTotalScore(){
-        double scores = 0;
-
-        for(ExamScore exScore : this._grades){
-            scores += exScore.getScore();
-        }
-
-        return scores;
-    }
-
-    public void displayInfo(){
-        System.out.println(this.name + " had a total of " + this.getTotalScore() + ". He had an average of " + this.calculateAverage());
-    }
-
-    public ExamScore getExamScore(String courseCode){
-        for(ExamScore exScore : this._grades){
-            if(exScore.getCourseCode().equals(courseCode)){
-                return exScore;
-            }
-        }
-
-        return null;
-    }
-
-    public void enterScore(String courseCode, double score){
-            ExamScore existingExamScore = this.getExamScore(courseCode);
-            if(existingExamScore != null){
-                existingExamScore.setScore(score);
-            }else{
-                this._grades.add(new ExamScore(courseCode,score));
-            }
-    }
 
 }
